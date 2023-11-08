5daad4c0f8eb8d33e21dc64fb229d89ca62eed5a
Kristina Dimitrieva <98698205+FroschMadchen@users.noreply.github.com>
2023-11-08 23:24:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/taskbia/authorization/ActivityAuthorization.kt
@@ -0,0 +1,15 @@
+package com.example.taskbia.authorization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.example.taskbia.R
+
+class ActivityAuthorization : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/taskbia/authorization/FragmentEnteringPassword.kt
@@ -0,0 +1,24 @@
+package com.example.taskbia.authorization
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import com.example.taskbia.R
+
+class FragmentEnteringPassword : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_entering_password, container, false)
+    }
+
+
+    }
