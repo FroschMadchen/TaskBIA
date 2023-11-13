f4bbcd716b8b194c480a67868edc980313f7d7a9
Kristina Dimitrieva <98698205+FroschMadchen@users.noreply.github.com>
2023-11-13 19:37:56 +0300

    1.3

---
+++ app/src/main/java/com/example/taskbia/authorization/adapter/Task.kt
@@ -0,0 +1,12 @@
+package com.example.taskbia.authorization.adapter
+
+data class Task(
+    val text1:String,
+    val text2:String,
+    val text3:String,
+    val text4:String,
+    val text5:String,
+    val image:Int
+
+
+)
